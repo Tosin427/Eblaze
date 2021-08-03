665a1483721c57c3529c43b718cdcc8a760ffc58
Oluwatosin Ajiboye <48648548+Tosin427@users.noreply.github.com>
2021-08-03 13:01:42 +0100

    corrections

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use("/api/auth", require("./routes/api/auth"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
+
+  // app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
