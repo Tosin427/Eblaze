147a1c6bb995279a4f0fc31432901564bb6b1845
Oluwatosin Ajiboye <48648548+Tosin427@users.noreply.github.com>
2021-08-12 22:44:58 +0100

    layout

--- client/src/components/layout2/layout2.js
+++ client/src/components/layout2/layout2.js
@@ -2,30 +2,26 @@ import React, { useState } from "react";
 import "./layout.css";
 import logo from "../../img/logo.png";
 import {
-
-    WechatOutlined,
-    SettingOutlined,
-    AreaChartOutlined,
-    NodeExpandOutlined,
-    PartitionOutlined,
-    PullRequestOutlined,
-    WalletOutlined,
-    LogoutOutlined,
-  } from "@ant-design/icons";
-import { Link } from 'react-router-dom';
+  WechatOutlined,
+  SettingOutlined,
+  AreaChartOutlined,
+  NodeExpandOutlined,
+  PartitionOutlined,
+  PullRequestOutlined,
+  WalletOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 // import SideMenu from './side-menu/SideMenu';
-import SideMenuLogo from './side-menu-logo/SideMenuLogo';
-import SideMenuList from './side-menu/SideMenuList';
-import SideMenuItem from './side-menu-item/SideMenuItem';
-
-
+import SideMenuLogo from "./side-menu-logo/SideMenuLogo";
+import SideMenuList from "./side-menu/SideMenuList";
+import SideMenuItem from "./side-menu-item/SideMenuItem";
 
 const initial_state = {
   navCollapsed: false,
   activeMenu: null,
 };
-
 
 const Layout = ({ children, logout, user }) => {
   const [state, setState] = useState(initial_state);
@@ -45,37 +41,36 @@ const Layout = ({ children, logout, user }) => {
         </div>
 
         <ul className="menu-list">
-        <li className="menu-item active">
-                <WalletOutlined /> <Link to="/newdashboard">Overview</Link>
-            </li>
-            <li className="menu-item">
-                <WalletOutlined /> <Link to="/wallet">Wallet</Link>
-            </li>
-            <li className="menu-item">
-                <PartitionOutlined /> <Link to="/"> P2P</Link>
-            </li>
-            <li className="menu-item">
-                <PullRequestOutlined /> <Link to="/transactions">Transactions</Link>
-            </li>
-            <li className="menu-item">
-                <NodeExpandOutlined /> <Link to="/">Trade Now</Link>
-            </li>
-            <li className="menu-item">
-                <AreaChartOutlined /> <Link to="/rates"> Rates</Link>
-            </li>
-            {/* <li className="menu-item">
+          <li className="menu-item active">
+            <WalletOutlined /> <Link to="/newdashboard">Overview</Link>
+          </li>
+          <li className="menu-item">
+            <WalletOutlined /> <Link to="/wallet">Wallet</Link>
+          </li>
+          <li className="menu-item">
+            <PartitionOutlined /> <Link to="/"> P2P</Link>
+          </li>
+          <li className="menu-item">
+            <PullRequestOutlined /> <Link to="/transactions">Transactions</Link>
+          </li>
+          <li className="menu-item">
+            <NodeExpandOutlined /> <Link to="/">Trade Now</Link>
+          </li>
+          <li className="menu-item">
+            <AreaChartOutlined /> <Link to="/rates"> Rates</Link>
+          </li>
+          {/* <li className="menu-item">
                 <WechatOutlined /> <Link to="/chat"> Chat</Link>
             </li> */}
-            <li className="menu-item">
-                <SettingOutlined /> <Link to="/settings"> Settings</Link>
-            </li>
-            <li className="menu-item">
-
-                <LogoutOutlined /> <a href="javascript:;" onClick={logout} id='nav-toggle'>Logout</a>
-
-            </li>
-
-       
+          <li className="menu-item">
+            <SettingOutlined /> <Link to="/settings"> Settings</Link>
+          </li>
+          <li className="menu-item">
+            <LogoutOutlined />{" "}
+            <a href="javascript:;" onClick={logout} id="nav-toggle">
+              Logout
+            </a>
+          </li>
         </ul>
         <a
           href="javascript:;"
@@ -90,28 +85,30 @@ const Layout = ({ children, logout, user }) => {
         className={`dashboard-wrapper ${state.navCollapsed ? "collapsed" : ""}`}
       >
         <div className="header">
-
-            <span  onClick={toggleNavCollapse}  className='mobile-nav-toggle'>
-                <i className='fa fa-chevron-right'></i>
-            </span>
-            <span className="name-abbv">
-                {user && user.name && user.name.split(' ')[0] && user.name.split(' ')[0].slice(0,1) }
-                {user && user.name && user.name.split(' ')[1] && user.name.split(' ')[1].slice(0,1) }
-            </span>
-            <a href="">{user && user.name}</a>
-
+          <span onClick={toggleNavCollapse} className="mobile-nav-toggle">
+            <i className="fa fa-chevron-right"></i>
+          </span>
+          <span className="name-abbv">
+            {user &&
+              user.name &&
+              user.name.split(" ")[0] &&
+              user.name.split(" ")[0].slice(0, 1)}
+            {user &&
+              user.name &&
+              user.name.split(" ")[1] &&
+              user.name.split(" ")[1].slice(0, 1)}
+          </span>
+          <a href="">{user && user.name}</a>
         </div>
         <div className="bread-crumb">
-            <i className="fa fa-user"></i> User/{user && user.email}
+          <i className="fa fa-user"></i> User/{user && user.email}
         </div>
-        <div className="dashboard-content">
-            {children}
-
-        </div>
-        
+        <div className="dashboard-content">{children}</div>
       </div>
     </>
   );
 };
 
 export default Layout;
+
+//Main Layout2
