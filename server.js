62e2c5102319e8f10139941993afe6cc22903f14
Oluwatosin Ajiboye <48648548+Tosin427@users.noreply.github.com>
2021-08-15 12:40:01 +0100

    server

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ if (process.env.NODE_ENV === "production") {
 
   // app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  app.use(express.static("client/build"));
-
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    app.use(express.static("client/build"));
+
+    // app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
